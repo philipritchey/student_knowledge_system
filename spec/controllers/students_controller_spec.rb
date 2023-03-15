c4c054ebacdf253c59a2bbe6f0f9bdb16216412a
Ethan <ethannovicio@gmail.com>
2023-03-15 02:53:34 +0000

    history controller test case

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,22 +1,4 @@
 require 'rails_helper'
-# @routes = Rails.application.routes
-
-
-
-# RSpec.configure do |config|
-#   config.include Devise::Test::ControllerHelpers, type: :controller
-# end
-
-
-# def sign_in(user = double('user'))
-#   if user.nil?
-#     allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, {:scope => :user})
-#     allow(controller).to receive(:current_user).and_return(nil)
-#   else
-#     allow(request.env['warden']).to receive(:authenticate!).and_return(user)
-#     allow(controller).to receive(:current_user).and_return(user)
-#   end
-# end
 
 RSpec.describe StudentsController, type: :controller do
   describe "#controller" do
@@ -29,13 +11,6 @@ RSpec.describe StudentsController, type: :controller do
       
       @student = Student.create(firstname:'Zebulun', lastname:'Oliphant', uin:'734826482', email:'zeb@tamu.edu', classification:'U2', major:'CPSC', teacher:'student@gmail.com')
     end
-    
-    # around(:each) do |example|
-    #   controller.class.skip_before_action :authenticate_user
-    #   example.run
-    #   controller.class.before_action :authenticate_user
-    # end
-    
 
     it "calls index successfully" do
       get :index, params: { id: @student }
