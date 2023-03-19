893d338fe68858de52252a4bf8d1687500d08405
Ethan <ethannovicio@gmail.com>
2023-03-19 05:12:20 +0000

    expect() within students_steps.rb

--- features/step_definitions/students_steps.rb
+++ features/step_definitions/students_steps.rb
@@ -10,17 +10,19 @@
   Then('I submit the form') do
     find("[value='Filter Students List']").click
     expect(page).to have_content("Kunal")
-    # expect(students.find_by(firstname: "Kunal")).to be(successful)
   end
 
   When('I click show this student') do
+    expect(page).to have_selector("button", text: "Show this student")
     first("button", text: "Show this student").click
   end
 
   When('I fill in student {string} with {string}') do |string, string2|
     fill_in "student[#{string}]", with: string2
+    expect(find_field("student[#{string}]").value).to eq(string2)
   end
   
   When('I select {string} under tag') do |string|
     find("#selected_tag").select(string)
+    expect(find("#selected_tag").value).to eq(string)
   end
