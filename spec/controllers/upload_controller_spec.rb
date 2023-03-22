e38b184840ce4350e9af9b77871ba702c5f67b0c
Ethan <ethannovicio@gmail.com>
2023-03-22 04:24:28 +0000

    upload controller test w/ .display files, home controller characterization

--- spec/controllers/upload_controller_spec.rb
+++ spec/controllers/upload_controller_spec.rb
@@ -23,7 +23,11 @@ RSpec.describe UploadController, type: :controller do
     end
   
     context "with valid file and contents" do
-      it "creates a new course and student entries" do
+      before do
+        @course1 = Course.create(course_name: "test course1", teacher: @user.email, section: "000", semester: "Fall 2001")
+      end
+
+      it "creates student entries for a zip with .jpg" do
         file = fixture_file_upload('ProfRitchey_Template.zip', 'application/zip')
         params = { file: file, course_temp: @course.course_name, section_temp: @course.section, semester_temp: @course.semester }
         post :parse, params: params
@@ -31,7 +35,43 @@ RSpec.describe UploadController, type: :controller do
         expect(kunal.firstname).to eq('Kunal')
         expect(flash[:notice]).to eq("Upload successful!")
       end
+
+      it "creates student entries for a zip with .display" do
+        file = fixture_file_upload('431_image_roster_with_chrome_files.zip', 'application/zip')
+        params = { file: file, course_temp: @course1.course_name, section_temp: @course1.section, semester_temp: @course1.semester }
+        post :parse, params: params
+        uploaded_student = Student.find_by firstname: 'Ethan'
+        expect(uploaded_student.firstname).to eq('Ethan')
+        expect(flash[:notice]).to eq("Upload successful!")
+      end
+
+      it "updates students that already exist" do
+        @prev_student = Student.create(firstname:'Ethan', lastname:'Novicio', uin: '12345578', email:'zeb@tamu.edu', classification:'U2', major:'CPSC', teacher:@user.email)
+        file = fixture_file_upload('431_image_roster_with_chrome_files.zip', 'application/zip')
+        params = { file: file, course_temp: @course1.course_name, section_temp: @course1.section, semester_temp: @course1.semester }
+        post :parse, params: params
+        uploaded_student = Student.find_by firstname: 'Ethan'
+        puts "ups: #{uploaded_student.inspect}"
+        expect(@prev_student.email).not_to eq(uploaded_student.email)
+        expect(flash[:notice]).to eq("Upload successful!")
+      end
     end
+
+    # context "with invalid file and contents" do
+    #   it "redirects when CSV column contents are different than expected" do
+    #     file = fixture_file_upload('Wrong_Cols.zip', 'application/zip')
+    #     params = { file: file, course_temp: @course.course_name, section_temp: @course.section, semester_temp: @course.semester }
+    #     post :parse, params: params
+    #     expect(flash[:notice]).to eq("CSV column contents are different than expected.")
+    #   end
+
+    #   it "redirects when number of images does not match number of students" do
+    #     file = fixture_file_upload('Wrong_imgs.zip', 'application/zip')
+    #     params = { file: file, course_temp: @course.course_name, section_temp: @course.section, semester_temp: @course.semester }
+    #     post :parse, params: params
+    #     expect(flash[:notice]).to eq("Number of images does not match number of students")
+    #   end
+    # end
   
   end
 end
